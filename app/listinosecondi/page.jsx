c83de6453e99c31425e78e5008d6826f2f255956
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2024-11-27 11:05:48 +0100

    .

--- app/listinosecondi/page.jsx
+++ app/listinosecondi/page.jsx
@@ -2,21 +2,23 @@ export default function ListinoSecondi() {
     return (
         <>
             <main className="flex p-6 gap-[20px] mb-[20px]">
-                <div id="secondi" className="w-[50%]">
-                    <h1 className="text-5xl font-bold text-center mb-[40px]"><u>SECOND COURSES</u></h1>
-                    <h2 className="font-bold text-3xl mb-5"><i><u>Meat based</u></i></h2>
+                <div id="secondi-piatti" className="w-[50%]">
+                    <h1 className="text-5xl font-bold text-center mb-[40px]"><u>MAIN COURSES</u></h1>
+                    
+                    {/* MEAT */}
+                    <h2 className="font-bold text-3xl mb-5"><i><u>Meat</u></i></h2>
                     <div className="flex justify-between">
-                        <p><b>SLICED BEEF ( approx. 350 g )</b></p>
+                        <p><b>SLICED BEEF (approx. 350g)</b></p>
                         <p><b>€ 16.00</b></p>
                     </div>
-                    ( With a bed of mixed salad, <b>Grana cheese</b> and gluten-free polenta )
+                    ( on a bed of mixed salad, <b>Grana cheese</b>, and gluten-free polenta )
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
                         <p><b>SLICED CHICKEN BREAST</b></p>
                         <p><b>€ 12.00</b></p>
                     </div>
-                    ( With a bed of mixed salad, cherry tomatoes, and gluten-free polenta )
+                    ( on a bed of mixed salad, cherry tomatoes, and gluten-free polenta )
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
@@ -27,7 +29,7 @@ export default function ListinoSecondi() {
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
-                        <p><b>ROAST BEEF STEAK with gluten-free polenta</b></p>
+                        <p><b>ROAST BEEF STEAK WITH GLUTEN-FREE POLENTA</b></p>
                         <p><b>€ 13.00</b></p>
                     </div>
                     <div className="mb-3"></div>
@@ -40,40 +42,38 @@ export default function ListinoSecondi() {
                     <div className="mb-8"></div>
 
                     {/* FISH */}
-
-                    <h2 className="font-bold text-3xl mb-5"><i><u>Fish based</u></i></h2>
+                    <h2 className="font-bold text-3xl mb-5"><i><u>Fish</u></i></h2>
                     <div className="flex justify-between">
                         <p><b>MIXED GRILLED FISH</b></p>
                         <p><b>€ 18.00</b></p>
                     </div>
-                    ( mixed fish and <b>shellfish and crustaceans</b> )
+                    ( mixed fish, <b>shellfish</b>, and <b>crustaceans</b> )
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
                         <p><b>GRILLED PRAWNS AND SCAMPI</b></p>
                         <p><b>€ 16.00</b></p>
                     </div>
-                    ( <b>crustaceans</b> )
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
-                        <p><b>GRILLED SEA BASS</b></p>
+                        <p><b>GRILLED SEABASS</b></p>
                         <p><b>€ 13.00</b></p>
                     </div>
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
-                        <p><b>MIXED FRIED FISH</b></p>
+                        <p><b>MIXED FRIED SEAFOOD</b></p>
                         <p><b>€ 17.00</b></p>
                     </div>
-                    ( <b>mollusks, crustaceans, flour</b> - calamari, cuttlefish, gobies, and polenta )
+                    ( mollusks, crustaceans, and flour )
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
                         <p><b>SLICED SWORDFISH</b></p>
                         <p><b>€ 16.00</b></p>
                     </div>
-                    ( swordfish, laid on a bed of salad, arugula, and tomato, with <b>balsamic vinegar</b> allergen )
+                    ( on a bed of salad, arugula, and cherry tomatoes, with <b>balsamic vinegar allergen</b> )
                     <div className="mb-3"></div>
 
                     <div className="flex justify-between">
@@ -84,60 +84,67 @@ export default function ListinoSecondi() {
                 </div>
 
                 {/* SIDE DISHES */}
-                <div>
-                    <div id="contorni">
-                        <h1 className="text-5xl font-bold text-center mb-[40px]"><u>SIDE DISHES</u></h1>
-                        <div className="flex justify-between">
-                            <p><b>FRENCH FRIES</b></p>
-                            <p><b>€ 3.00</b></p>
-                        </div>
-                        <div className="mb-3"></div>
-
-                        <div className="flex justify-between">
-                            <p><b>GRILLED VEGETABLES</b></p>
-                            <p><b>€ 4.50</b></p>
-                        </div>
-                        ( zucchini, eggplants, bell peppers )
-                        <div className="mb-3"></div>
-
-                        <div className="flex justify-between">
-                            <p><b>MIXED SALAD</b></p>
-                            <p><b>€ 3.00</b></p>
-                        </div>
-                        ( lettuce, carrots, radicchio, cherry tomatoes, corn )
-                        <div className="mb-8"></div>
+                <div id="contorni" className="w-[50%]">
+                    <h1 className="text-5xl font-bold text-center mb-[40px]"><u>SIDES</u></h1>
+                    <div className="flex justify-between">
+                        <p><b>FRIED POTATOES</b></p>
+                        <p><b>€ 4.50</b></p>
                     </div>
+                    <div className="mb-3"></div>
 
-                    {/* SALAD */}
-
-                    <div id="insalatone">
-                        <h1 className="text-5xl font-bold text-center mb-[40px]"><u>SALADS</u></h1>
-                        <div className="flex justify-between">
-                            <p><b>CAESAR SALAD</b></p>
-                            <p><b>€ 9.00</b></p>
-                        </div>
-                        ( romaine lettuce, <b>Grana cheese</b>, croutons, grilled chicken, Caesar dressing )
-                        <div className="mb-3"></div>
-
-                        <div className="flex justify-between">
-                            <p><b>GREEK SALAD</b></p>
-                            <p><b>€ 8.50</b></p>
-                        </div>
-                        ( mixed salad, cucumbers, tomatoes, red onions, olives, <b>feta cheese</b>, oregano, olive oil )
-                        <div className="mb-3"></div>
-
-                        <div className="flex justify-between">
-                            <p><b>MEDITERRANEAN SALAD</b></p>
-                            <p><b>€ 9.50</b></p>
-                        </div>
-                        ( mixed salad, cherry tomatoes, olives, tuna, <b>mozzarella cheese</b>, oregano, olive oil )
-                        <div className="mb-3"></div>
+                    <div className="flex justify-between">
+                        <p><b>MIXED SALAD</b></p>
+                        <p><b>€ 4.00</b></p>
                     </div>
+                    <div className="mb-3"></div>
+
+                    <div className="flex justify-between">
+                        <p><b>NAPLES-STYLE BROCCOLI RABE</b></p>
+                        <p><b>€ 4.50</b></p>
+                    </div>
+                    ( garlic, chili pepper, broccoli rabe )
+                    <div className="mb-3"></div>
+
+                    <div className="flex justify-between">
+                        <p><b>SAUTÉED SPINACH</b></p>
+                        <p><b>€ 4.00</b></p>
+                    </div>
+                    ( with garlic and chili pepper )
+                    <div className="mb-3"></div>
+
+                    <div className="flex justify-between">
+                        <p><b>GRILLED VEGETABLES</b></p>
+                        <p><b>€ 5.00</b></p>
+                    </div>
+                    ( eggplant, zucchini, and bell peppers )
+                    <div className="mb-8"></div>
+
+                    {/* SALADS */}
+                    <h1 className="text-4xl font-bold text-center mb-[40px]"><u>BIG SALADS</u></h1>
+                    <div className="flex justify-between">
+                        <p><b>MEDITERRANEAN SALAD</b></p>
+                        <p><b>€ 8.00</b></p>
+                    </div>
+                    ( <b>buffalo mozzarella</b>, green salad, tomatoes, corn, and tuna )
+                    <div className="mb-3"></div>
+
+                    <div className="flex justify-between">
+                        <p><b>CHARLIE SALAD</b></p>
+                        <p><b>€ 8.00</b></p>
+                    </div>
+                    ( green salad, arugula, tomatoes, <b>buffalo mozzarella</b>, and shrimp )
+                    <div className="mb-3"></div>
+
+                    <div className="flex justify-between">
+                        <p><b>CHICKEN SALAD</b></p>
+                        <p><b>€ 8.00</b></p>
+                    </div>
+                    ( green salad, arugula, cherry tomatoes, and crispy chicken )
+                    <div className="mb-3"></div>
                 </div>
             </main>
             <footer className="text-center text-[20px] font-bold">
                 <p>( the allergenic ingredients are highlighted in bold )</p>
-                <p>COVER CHARGE € 2.20</p>
             </footer>
         </>
     )
